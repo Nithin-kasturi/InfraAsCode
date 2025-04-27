e67f423cca6701cf38b3538cfd879b98ac12fcb2
Nithin-kasturi <107863285+Nithin-kasturi@users.noreply.github.com>
2025-04-27 13:27:53 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,6 +1,6 @@
-resource "aws_s3_bucket" "Bucket_for_code" {
-  bucket = "Bucket_for_code"
-  tags={
-    Name="Bucket_for_code"
+resource "aws_s3_bucket" "bucket_for_code" {
+  bucket = "bucket-for-code"
+  tags = {
+    Name = "bucket-for-code"
   }
 }
